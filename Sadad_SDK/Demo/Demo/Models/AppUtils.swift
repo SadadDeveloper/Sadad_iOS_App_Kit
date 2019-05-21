c7ce0273dfd7caff58400be40c17241cbe069b9b
hiren.rathod <hiren.rathod@sufalamtech.com>
2019-05-21 13:40:14 +0530

    first SDK commit

---
+++ Sadad_SDK/Demo/Demo/Models/AppUtils.swift
@@ -0,0 +1,90 @@
+//
+//  AppUtils.swift
+//  DemoImplementFramework
+//
+//  Created by Hiren on 12/04/19.
+//  Copyright © 2019 sufalam. All rights reserved.
+
+
+import UIKit
+
+class AppUtils: NSObject {
+    
+    static var arrProgressView : NSMutableArray = NSMutableArray();
+    var Loader: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    static func startLoading(_ view : UIView){
+        let viewForAI = UIView(frame: view.bounds);
+        
+        let activityIndicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        activityIndicatorView.backgroundColor = UIColor.white
+        activityIndicatorView.style = .gray
+        activityIndicatorView.startAnimating();
+        activityIndicatorView.center = viewForAI.center;
+        viewForAI.addSubview(activityIndicatorView);
+        view.addSubview(viewForAI);
+        self.arrProgressView.add(viewForAI);
+    }
+    
+    
+    static func stopLoading(){
+        for i in 0 ..< self.arrProgressView.count {
+            let progressObject : UIView! = self.arrProgressView.object(at: i) as? UIView;
+            if(progressObject != nil){
+                DispatchQueue.main.async {
+                    progressObject!.removeFromSuperview()
+                }
+            }else{
+                print("progressview is nil");
+            }
+        }
+        self.arrProgressView.removeAllObjects();
+    }
+    
+    static func convertDateToAnyFormatEnglish(sourceDateFormat:String,SourceDate:String,destinationDateFormat:String) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = sourceDateFormat
+        let date = dateFormatter.date(from: SourceDate)
+        dateFormatter.dateFormat = destinationDateFormat
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US") as Locale?
+        if date != nil{
+            let strDate = dateFormatter.string(from: date!)
+            return strDate
+        }else{
+            return ""
+        }
+        
+    }
+    
+    static func NsNumberToCurrencyFormate(_ Amount:NSNumber) -> String{
+        let formatter = NumberFormatter()
+        //formatter.locale = Locale.current // Change this to another locale if you want to force a specific locale, otherwise this is redundant as the current locale is the default already
+        formatter.locale = NSLocale(localeIdentifier: "en_US") as Locale!
+        formatter.currencySymbol = ""
+        formatter.numberStyle = .currency
+        if let formattedTipAmount = formatter.string(from: Amount as NSNumber) {
+            let strAmount =  "\(formattedTipAmount)"
+            return strAmount
+        }else{
+            let strAmount =  "\(Amount.doubleValue)"
+            return strAmount
+        }
+    }
+    
+    
+    static func ArrayToString(Array:NSMutableArray) -> String{
+        
+        if let theJSONData = try?  JSONSerialization.data(
+            withJSONObject: Array,
+            options: .prettyPrinted
+            ),
+            let theJSONText = String(data: theJSONData,
+                                     encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue)) {
+            return theJSONText
+        }
+        
+        return ""
+    }
+    
+    
+}
---
+++ Sadad_SDK/Demo/Pods/KBNumberPad/README.md
@@ -0,0 +1,70 @@
+# KBNumberPad (iOS)
+
+[![CI Status](http://img.shields.io/travis/kbiakov/KBNumberPad.svg?style=flat)](https://travis-ci.org/kbiakov/KBNumberPad)
+[![Version](https://img.shields.io/cocoapods/v/KBNumberPad.svg?style=flat)](http://cocoapods.org/pods/KBNumberPad)
+[![License](https://img.shields.io/cocoapods/l/KBNumberPad.svg?style=flat)](http://cocoapods.org/pods/KBNumberPad)
+[![Platform](https://img.shields.io/cocoapods/p/KBNumberPad.svg?style=flat)](http://cocoapods.org/pods/KBNumberPad)
+
+<b>KBNumberPad</b> is a customizable number pad as replacement of default.
+
+## Example
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+- Xcode 8.0+
+- iOS 9.0+
+- Swift 3.0+
+
+## Setup
+
+### CocoaPods
+<b>KBNumberPad</b> is available through [CocoaPods](http://cocoapods.org), dependency manager for Cocoa projects. To install it, run the command:
+```bash
+$ gem install cocoapods
+```
+
+To integrate <b>KBNumberPad</b> into your Xcode project using CocoaPods, specify it in your `Podfile`:
+```ruby
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target '<Your Target Name>' do
+    pod 'KBNumberPad', '~> 1.0.0'
+end
+```
+
+Then, run the following command:
+```bash
+$ pod install
+```
+
+## Usage
+You can customize base components of this number pad view as follows:
+```Swift
+let numberPad = KBNumberPad()
+numberPad.setDelimiterColor(UIColor.lightGray)
+numberPad.setButtonsColor(UIColor.black)
+numberPad.setButtonsBackgroundColor(UIColor.white)
+```
+
+This snippet shows that all buttons grouped, but there are methods to do setup separately like:
+```Swift
+numberPad.setNumberButtonsColor(UIColor.black)
+numberPad.setClearButtonColor(UIColor.darkGray)
+numberPad.setDoneButtonColor(UIColor.darkGray)
+```
+
+There is params to customize: it's __color__ of grouped or all buttons, __font__ & __background__.
+Also you can change the __icon__ for clear button.
+
+## Screenshots
+[![2017-05-14_20.29.02.png](https://s3.postimg.org/r1fswvhg3/2017-05-14_20.29.02.png)](https://postimg.org/image/7wcjn42rz/)
+[![2017-05-14_20.51.04.png](https://s13.postimg.org/f7akftz3b/2017-05-14_20.51.04.png)](https://postimg.org/image/qwek3sq1v/)
+[![2017-05-14_20.59.17.png](https://s12.postimg.org/518601cm5/2017-05-14_20.59.17.png)](https://postimg.org/image/jx6p7mo0p/)
+
+## Author
+### [Kirill Biakov](https://github.com/kbiakov)
+
+## License
+<b>KBNumberPad</b> is available under the [Apache License 2.0](https://github.com/kbiakov/KBNumberPad/blob/master/LICENSE) license.
